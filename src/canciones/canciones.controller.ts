c4cb2c58fac64d86cf24adb448eec99cccd31652
Juan <juan.maldonado.leon@gmail.com>
2024-07-23 19:04:13 -0400

    funcion de obtener todas las canciones

--- src/canciones/canciones.controller.ts
+++ src/canciones/canciones.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CancionesService } from './canciones.service';
 
 @Controller('canciones')
-export class CancionesController {}
+export class CancionesController {
+
+    constructor(private readonly cancionesService:CancionesService){}
+
+    @Get()
+    obtenerCanciones(){
+        return this.cancionesService.obtenerCanciones();
+    }
+}
