319d65d10edf435f8afd30b24cfcbc222c1f4c88
Juan Francisco <juan.maldonado.leon@gmail.com>
2025-07-18 11:03:52 -0400

    cambios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Artista } from './artista';
+import { Playlist } from './playlist';
+import { Cancion } from './cancion';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('artistas')
+  getArtistas(): Artista[] {
+    return this.appService.getArtistas();
+  }
+
+  @Get('canciones')
+  getCanciones(): Cancion[] {
+    return this.appService.getCanciones();
+  }
+
+  @Get('playlists')
+  getPlaylists(): Playlist[] {
+    return this.appService.getPlaylist();
+  }
+
+  @Post('artistas')
+  crearArtista(@Body() artista: Artista) {
+    return this.appService.crearArtista(artista);
+  }
+
+  @Post('canciones')
+  crearCancion(@Body() cancion: Cancion) {
+    return this.appService.crearCancion(cancion);
+  }
+
+  @Post('playlists')
+  crearPlaylist(@Body() playlist: Playlist, @Res() response: Response) {
+    const resultado = this.appService.crearPlaylist(playlist);
+    if (resultado == 0) {
+      response.status(HttpStatus.OK).send();
+    } else {
+      response.status(HttpStatus.BAD_REQUEST).send();
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { Artista } from './artista';
+import { Playlist } from './playlist';
+import { Cancion } from './cancion';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  artistas: Artista[] = [];
+  canciones: Cancion[] = [];
+  playlists: Playlist[] = [];
+
+  getCanciones(): Cancion[] {
+    return this.canciones;
+  }
+
+  getArtistas(): Artista[] {
+    return this.artistas;
+  }
+
+  getPlaylist(): Playlist[] {
+    return this.playlists;
+  }
+
+  crearCancion(cancion: Cancion) {
+    this.canciones.push(cancion);
+  }
+
+  crearArtista(artista: Artista) {
+    this.artistas.push(artista);
+  }
+
+  crearPlaylist(playlist: Playlist): number {
+    for (let i = 0; i < this.playlists.length; i++) {
+      if (this.playlists[i].nombre === playlist.nombre) {
+        return 1;
+      }
+    }
+    this.playlists.push(playlist);
+    return 0;
   }
 }
